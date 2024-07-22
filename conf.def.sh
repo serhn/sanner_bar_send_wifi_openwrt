ada8ffdda9f029a6536e2a09b8e2ec452c8e7c78
Serhii Nikolaichuk <nikolserh@gmail.com>
2024-07-23 01:55:51 +0300

    add env and fix code

--- conf.def.sh
+++ conf.def.sh
@@ -1,2 +1,8 @@
-URL="URL_SEND_DATA" #url send code example "https://example.com"
-SCANNER_DONE="SECRET_KEY" #example "saiwoh1aeva3Hei1OhxoeYah4ihae3ae" 
+
+export LAMP_GREEN=/sys/class/leds/tp-link\:blue\:system/brightness
+export LAMP_RED=/sys/class/leds/tp-link\:blue\:system/brightness
+
+export URL="https://URL_SEND_DATA/api/scan/done"
+export SCANNER_DONE="SECRET_KEY" #example "saiwoh1aeva3Hei1OhxoeYah4ihae3ae" 
+
+export LINE_END="KEY_ENTER"
--- read.sh
+++ read.sh
@@ -1,34 +1,38 @@
-#/bin/sh
-#ps w | grep read.sh | grep -v grep | wc -l
-#PS_NUM=`ps w | grep read.sh | grep -v grep | wc -l`
-#echo $PS_NUM
-#if [ $PS_NUM > 2 ]; then
-#echo "Alreade running proces"
-#exit
-#fi
-./conf.sh
+#!/bin/sh
+PS_NUM=`ps w | grep read.sh | grep -v grep | wc -l`
+echo $PS_NUM
+if [ "$PS_NUM" -gt "2" ]; then
+  echo "Alreade running proces"
+  exit
+fi
+. ./conf.sh
+
 ./led/stop.sh
 ./led/red_on.sh&
 CODE=""
 thd --dump /dev/input/event0 |  awk '{ if($1!="#" && $3==1)  print $2}' | while read line
 do
-  if [ "$line" = "KEY_KPENTER" ]; then
+  if [ "$line" = "$LINE_END" ]; then
  echo -n $CODE
  echo ""
- ./led/stop.sh
- ./led/red_blink.sh&
- RESULT=`wget -q --no-check-certificate  -O - "$URL/scan/$SCANNER_DONE/$CODE"`
- ./led/stop.sh
+./led/stop.sh
+#./led/red_blink.sh&
+ CODE=`echo $CODE | cut -c 3-`
+ URL_FULL="$URL/$SCANNER_DONE/$CODE"
+ echo $URL_FULL
+ RESULT=`wget -q --no-check-certificate  -O - $URL_FULL`
+ echo $RESULT
+./led/stop.sh
 if [ "$RESULT" = "OK" ]; then
 ./led/code_ok.sh  
  else
    if [ "$RESULT" = "ALREADY" ]; then
-     ./led/code_ok_already.sh  
+./led/code_ok_already.sh  
    else
      if [ "$RESULT" = "NOT" ]; then
-       ./led/code_not.sh  
+./led/code_not.sh  
      else
-        ./led/red_on.sh&
+./led/red_on.sh&
      fi
   fi
 fi
